0ce24472607f08cd998afe6428868d024820b339
Pelidyai <dima-pile@mail.ru>
2023-01-07 19:59:27 +0700

    Improve Niche-objects

---
+++ market/constants.py
@@ -0,0 +1,1 @@
+samples_count: int = 20
--- market/infrastructure.py
+++ market/infrastructure.py
@@ -1,25 +1,54 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from market.items import Product
-from market.items import ClientProduct
-from market.items import MarketplaceProduct
+
+import numpy as np
+from numpy import ndarray
+
+from .constants import samples_count
+from .items import Product
+from .items import ClientProduct
+from .items import MarketplaceProduct
 
 
 @dataclass
 class Niche(ABC):
     name: str
+    commission: float
+    logistic_price: int
+    returned_percent: float
     products: list[Product]
 
     def __str__(self) -> str:
         return self.name
 
+    def __post_init__(self):
+        self.cost_data: ndarray = np.array([product.cost for product in self.products])
+
+    def get_concurrent_margin(self,
+                              mid_cost: float,
+                              unit_cost: int,
+                              unit_storage_cost: int) -> int:
+        return int(mid_cost - unit_cost - self.commission * mid_cost - self.logistic_price - unit_storage_cost)
+
+    def get_mean_concurrent_cost(self,
+                                 unit_cost: int,
+                                 storage_price: int) -> int:
+        keys: list[int] = []
+        step: int = len(self.cost_data) // samples_count
+        for i in range(samples_count - 1):
+            keys.append(i * step)
+        keys.append(len(self.cost_data) - 1)
+        for i in range(1, len(keys)):
+            concurrent_margin: int = self.get_concurrent_margin(self.cost_data[keys[i - 1]:keys[i]].mean(), unit_cost,
+                                                                storage_price)
+            if concurrent_margin > 0:
+                return int(self.cost_data[keys[i - 1]:keys[i]].mean())
+        return int(self.cost_data[-2:-1].mean())
+
 
 @dataclass
 class MarketplaceNiche(Niche):
     products: list[MarketplaceProduct]
-
-    def __str__(self) -> str:
-        return self.name
 
 
 @dataclass
