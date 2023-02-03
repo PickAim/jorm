f88bbd1629e33999a482b5a011635d4db3050237
Pelidyai <dima-pile@mail.ru>
2023-02-03 21:21:41 +0700

    Move tokenizer to Backend

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -2,8 +2,6 @@ import datetime
 import unittest
 from datetime import date
 
-from jorm.utils.tokens import Tokenizer
-
 from jorm.market.infrastructure import Warehouse, Address, HandlerType
 from jorm.market.items import ClientProduct, ProductHistoryUnit, ProductHistory
 from jorm.market.service import FrequencyResult, FrequencyRequest
@@ -35,16 +33,6 @@ class MyTestCase(unittest.TestCase):
         hashed = hasher.hash(password)
         print(hashed)
         self.assertTrue(hasher.verify(password, hashed))
-
-    def test_tokens_lifetime(self):
-        tokenizer = Tokenizer()
-        access_token = tokenizer.create_access_token(123, datetime.timedelta(microseconds=1))
-        update_token = tokenizer.create_update_token(123)
-        imprint_token = tokenizer.create_imprint_token(123)
-
-        self.assertTrue(tokenizer.is_token_expired(access_token))
-        self.assertFalse(tokenizer.is_token_expired(update_token))
-        self.assertFalse(tokenizer.is_token_expired(imprint_token))
 
 
 if __name__ == '__main__':
