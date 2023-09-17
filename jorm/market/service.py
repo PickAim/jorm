e5649714ec62a9d69c9320da9b460e0f74f7fb02
Pelidyai <dima-pile@mail.ru>
2023-09-17 15:43:26 +0700

    Correct request types. Use warehouse id instead of name

--- jorm/market/service.py
+++ jorm/market/service.py
@@ -16,21 +16,15 @@ class Request:
 
 
 @dataclass
-class NicheRequest(Request):
+class SimpleEconomyRequest(Request):
     niche_id: int
-    category_id: int
-    marketplace_id: int
-
-
-@dataclass
-class SimpleEconomyRequest(NicheRequest):
     product_exist_cost: int  # user defined cost for product
     cost_price: int  # how much it cost for user
-    length: int
-    width: int
-    height: int
-    mass: int
-    target_warehouse_name: str
+    length: float
+    width: float
+    height: float
+    mass: float
+    target_warehouse_id: int
 
 
 @dataclass
