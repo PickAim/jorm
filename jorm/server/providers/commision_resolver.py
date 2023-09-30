df5f99ab48b67b205c8a63f84e606fdd2243a82d
kostayli <99755309+kostayli@users.noreply.github.com>
2023-10-01 02:41:45 +0700

    Removed unnecessary functions described in jser

--- jorm/server/providers/commision_resolver.py
+++ jorm/server/providers/commision_resolver.py
@@ -6,54 +6,9 @@ from jorm.market.infrastructure import HandlerType
 
 class CommissionResolver(ABC):
     def __init__(self):
-        self._commission_data: dict = self.__template_get_commission_data()
-        self._warehouse_data: dict = self.__template_get_warehouse_data()
+        self._commission_data: dict
+        self._warehouse_data: dict
 
-    @abstractmethod
-    def update_commission_file(self, filepath: str) -> None:
-        pass
-
-    @abstractmethod
-    def update_warehouse_file(self, filepath: str) -> None:
-        pass
-
-    @abstractmethod
-    def get_commision_binary_path(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_commision_csv_path(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_warehouse_binary_path(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_warehouse_file_path(self) -> str:
-        pass
-
-    @abstractmethod
-    def _get_commission_data(self, binary_path: str) -> dict[str, any]:
-        pass
-
-    @abstractmethod
-    def _get_warehouse_data(self, binary_path: str) -> dict[str, any]:
-        pass
-
-    def __template_get_commission_data(self) -> dict[str, any]:
-        binary_path: str = self.get_commision_binary_path()
-        if not os.path.exists(binary_path):
-            csv_path: str = self.get_commision_csv_path()
-            self.update_commission_file(csv_path)
-        return self._get_commission_data(binary_path)
-
-    def __template_get_warehouse_data(self) -> dict[str, any]:
-        binary_path: str = self.get_warehouse_binary_path()
-        if not os.path.exists(binary_path):
-            csv_path: str = self.get_warehouse_file_path()
-            self.update_warehouse_file(csv_path)
-        return self._get_warehouse_data(binary_path)
 
     @abstractmethod
     def _get_commission_for_niche(self, niche_name: str) -> dict[str, float]:
@@ -68,6 +23,6 @@ class CommissionResolver(ABC):
         pass
 
     @abstractmethod
-    def get_commision_for_warehouse(self, warehouse_id: str) -> float:
+    def get_data_for_warehouse(self, warehouse_id: str) -> float:
         pass
 
